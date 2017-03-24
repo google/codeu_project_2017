7fc14c95262e17f1d1a957384d0566ad0b207264
itsStrobe <josezavala0297@gmail.com>
2017-03-24 14:56:07 -0600

    Added scripts for Database Creation

---
+++ create_database.sh
@@ -0,0 +1,3 @@
+
+
+java -cp ./third_party/sqlite-jdbc-3.16.1.jar:./bin codeu.chat.codeU_db.CreateDatabase
---
+++ src/codeu/chat/codeU_db/CreateDatabase.java
@@ -0,0 +1,91 @@
+package codeu.chat.codeU_db;
+
+import java.sql.*;
+
+public class CreateDatabase
+{
+    public static void main( String args[] )
+    {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:./bin/codeu/chat/codeU_db/ChatDatabase.db");
+            System.out.println("Opened database successfully");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+
+        try {
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE USERS " +
+                    "(ID            INT PRIMARY KEY NOT NULL," +
+                    " NAME          CHAR(25)        NOT NULL, " +
+                    " TimeCreated   TIMESTAMP       NOT NULL, " +
+                    " PASSWORD      TEXT            NOT NULL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table <USERS> created successfully");
+
+        try {
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE CONVERSATIONS " +
+                    "(ID            INT PRIMARY KEY NOT NULL, " +
+                    " NAME          CHAR(25)        NOT NULL, " +
+                    " TimeCreated   TIMESTAMP       NOT NULL, " +
+                    " OWNERID       INT             NOT NULL, " +
+                    " FOREIGN KEY(OWNERID) REFERENCES USERS(ID))";
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table <CONVERSATIONS> created successfully");
+
+        try {
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE USER_CONVERSATION " +
+                    "(ID                INT PRIMARY KEY NOT NULL, " +
+                    " USERID            INT             NOT NULL, " +
+                    " CONVERSATIONID    INT             NOT NULL, " +
+                    " FOREIGN KEY(USERID) REFERENCES USERS(ID), " +
+                    " FOREIGN KEY(CONVERSATIONID) REFERENCES CONVERSATIONS(ID))";
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table <USER_CONVERSATION> created successfully");
+
+        try {
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE MESSAGES " +
+                    "(ID                INT PRIMARY KEY NOT NULL, " +
+                    " USERID            INT             NOT NULL, " +
+                    " CONVERSATIONID    INT             NOT NULL, " +
+                    " TimeCreated       TIMESTAMP       NOT NULL, " +
+                    " MESSAGE           TEXT            NOT NULL, " +
+                    " FOREIGN KEY(USERID) REFERENCES USERS(ID), " +
+                    " FOREIGN KEY(CONVERSATIONID) REFERENCES CONVERSATIONS(ID))";
+            stmt.executeUpdate(sql);
+            stmt.close();
+
+            c.close();
+
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table <MESSAGES> created successfully");
+
+        System.out.println("Finished Creating Database Successfully!!!");
+    }
+}
