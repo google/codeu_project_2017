0bddfba7c690a123f33f238f6d40553ff1882d02
itsStrobe <josezavala0297@gmail.com>
2017-03-24 14:56:07 -0600

    Added scripts for Database Creation

--- make.sh
+++ make.sh
@@ -18,5 +18,5 @@ set -e
 
 mkdir -p bin
 
-javac -Xlint $(find * | grep "\\.java$") -d ./bin -sourcepath ./src -cp ./third_party/junit4.jar:./bin
-javac -Xlint $(find * | grep "\\.java$") -d ./bin -sourcepath ./test -cp ./third_party/junit4.jar:./bin
+javac -Xlint $(find * | grep "\\.java$") -d ./bin -sourcepath ./src -cp ./third_party/junit4.jar:./third_party/sqlite-jdbc-3.16.1.jar:./bin
+javac -Xlint $(find * | grep "\\.java$") -d ./bin -sourcepath ./test -cp ./third_party/junit4.jar:./third_party/sqlite-jdbc-3.16.1.jar:./bin
