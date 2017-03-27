dcc022e1de20a30a95ec74d7119901f96bae4eae
César Arturo González <arthurgonzalezp@gmail.com>
2017-03-27 13:36:56 -0600

    Small changes in DatabaseUser and in database

--- src/codeu/chat/common/DatabaseUser.java
+++ src/codeu/chat/common/DatabaseUser.java
@@ -1,20 +1,49 @@
 package codeu.chat.common;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import codeu.chat.util.Serializer;
+import codeu.chat.util.Serializers;
 public class DatabaseUser{
-    private String username;
+    /*public static final Serializer<DatabaseUser> SERIALIZER = new Serializer<DatabaseUser>() {
+
+        @Override
+        public void write(OutputStream out, DatabaseUser value) throws IOException {
+
+        Serializers.STRING.write(out, value.display_name);
+        Serializers.STRING.write(out, value.name);
+        Serializers.STRING.write(out, value.pswd);
+
+        }
+
+        @Override
+        public User read(InputStream in) throws IOException {
+
+        return new User(
+            Serializers.STRING.read(in),
+            Serializers.STRING.read(in),
+            Serializers.STRING.read(in)
+        );
+
+        }
+    };*/
+
+    private String display_name;
     private String pswd;
     private String name;
 
-    public DatabaseUser(String username, String pswd, String name){
+    public DatabaseUser(String display_name, String pswd, String name){
         this.name = name;
-        this.username = username;
+        this.display_name = display_name;
         this.pswd = pswd;
     }
 
     public boolean checkLogin(String loginUsername, String loginPswd){
-        return this.username.equals(loginUsername) && this.pswd.equals(loginPswd);
+        return this.name.equals(loginUsername) && this.pswd.equals(loginPswd);
     }
     public String toString(){
-        return "Name: " + this.name + "\nUsername: " + this.username;
+        return "Username: " + this.name + "\nDisplay name: " + this.display_name;
     }
 }
