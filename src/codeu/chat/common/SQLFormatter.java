f0c6e27ddab5628b8a59cdbfd17f01591192caf3
itsStrobe <josezavala0297@gmail.com>
2017-04-06 19:57:38 -0500

    Started DB implementation. Check if this works for you guys.

---
+++ src/codeu/chat/common/SQLFormatter.java
@@ -0,0 +1,7 @@
+package codeu.chat.common;
+
+/**
+ * Created by strobe on 4/04/17.
+ */
+public class SQLFormatter {
+}
---
+++ src/codeu/chat/server/ViewDatabase.java
@@ -0,0 +1,7 @@
+package codeu.chat.server;
+
+/**
+ * Created by strobe on 4/04/17.
+ */
+public class ViewDatabase {
+}
