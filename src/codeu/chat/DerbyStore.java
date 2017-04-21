a1ea5939a14d8479c6d6f4144f2ce05c35aaaee8
Malik Graham <malikgraham15@yahoo.com>
2017-04-20 23:52:40 -0500

    Revert "Merge develop with master"

--- src/codeu/chat/DerbyStore.java
+++ src/codeu/chat/DerbyStore.java
@@ -1,7 +1,6 @@
 package codeu.chat;
 
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,41 +13,19 @@ import codeu.chat.common.Time;
 import codeu.chat.common.User;
 //import codeu.chat.common.Uuid;
 import codeu.chat.util.store.Store;
-import codeu.chat.util.Logger.Log;
-import codeu.chat.util.Logger;
+
 import codeu.chat.util.Uuid;
 
-import java.io.File;
+
 import java.sql.Connection;
 
 public class DerbyStore {
 	
-	private String driver = "org.apache.derby.jdbc.EmbeddedDriver";
-	private String protocol = "jdbc:derby:test";
+	public String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+	public String protocol = "jdbc:derby:test";
 	
-	private Connection conn;
-	private Statement stmt;
-	
-	private final String userTableName = "userInfo";
-	private final String conversationTableName = "conversation";
-	private final String messageTableName = "message";
-	private final String chatParticipantsTableName = "chatParticipants";
-	
-	private final String checkUsername = "SELECT * FROM " + userTableName + " WHERE name = ?";
-	private final String checkValidUser = "SELECT * FROM " + userTableName + " WHERE name = ? AND password = ?";
-	
-	private final String addUserInfo = "INSERT INTO " + userTableName + " (id, name, password, creation) values (?, ?, ?, ?)";
-	private final String addConversationInfo = "INSERT INTO " + conversationTableName + " (id, owner, creation, title, firstMessage, lastMessage) " + 
-			"values (?, ?, ?, ?, ?, ?)";
-	private final String addMessageInfo = "INSERT INTO " + messageTableName + " (id, previous, creation, author, content, nextMessage) " + 
-	"values (?, ?, ?, ?, ?, ?)";
-	private final String addChatParticipantsInfo = "INSERT INTO " + chatParticipantsTableName + " (conversationid, userid) values (?, ?)";
-
-	
-	private final String updateLastMessageById = "UPDATE " + messageTableName + " SET nextMessage = ? WHERE id = ?";
-	private final String updateConversationById = "UPDATE " + conversationTableName + " SET firstMessage = ?, lastMessage = ? WHERE id = ?";
-	
-	private static final Logger.Log LOG = Logger.newLog(ServerMain.class);
+	Connection conn;
+	Statement stmt;
 	
 	
 	public DerbyStore() {
@@ -57,92 +34,59 @@ public class DerbyStore {
 			// Load the class necessary
 			Class.forName(driver).newInstance();
 			
-			// Checks to see if the database directory exist
-			File database = new File("testchatapp");
-					
-			// If it does exist then we connect to it, while
-			// not overwriting data.
-			if (database.exists() && database.isDirectory()) {
-				conn = DriverManager.getConnection(protocol + "chatapp;", null);
-				stmt = conn.createStatement();
-				LOG.info("Tables exist. Connection made.");
-				return;
-			}
-	
-			// Connect to the database while creating a new schema
+			// Connect to the Database
 			conn = DriverManager.getConnection(protocol + "chatapp;create=true", null);
 			
 			// Create a statement object to send queries
 			stmt = conn.createStatement();
 			
-			// Create the chat user table
-			stmt.execute("CREATE TABLE " + userTableName + "(id varchar(255), name varchar(255), password varchar(255), creation BIGINT)");
+			// If this table exists then we just continue
+			// since we don't want to create tables that exist
+			stmt.execute("SELECT * FROM chatuser");
 			
-			// Create the conversations table
-			stmt.execute("CREATE TABLE " + conversationTableName + "(id varchar(255), "
-					+ "owner varchar(255), creation BIGINT, title varchar(255), users varchar(255), firstMessage varchar(255)," +
-			"lastMessage varchar(255))");
-			
-			// Create the message table
-			stmt.execute("CREATE TABLE " + messageTableName + "(id varchar(255),"
-					 + "previous varchar(255), creation BIGINT, author varchar(255), content varchar(255), nextMessage varchar(255))");
-			
-			stmt.execute("CREATE TABLE " + chatParticipantsTableName + "(conversationid varchar(255), userid varchar(255))");
-			
-			// Give confirmation of execution.
-			LOG.info("Tables do not exist. Table creation executed.");
+			// Give confirmation of connection
+			System.out.println("Tables exist. Connection made.");
 		}
 		catch (Exception ex) {
+						try {
+						// Create the chat user table
+						stmt.execute("CREATE TABLE chatuser(id varchar(255), name varchar(255), password varchar(255), creation BIGINT)");
+						
+						// Create the conversations table
+						stmt.execute("CREATE TABLE conversation(id varchar(255), "
+								+ "owner varchar(255), creation BIGINT, title varchar(255), users varchar(255), firstMessage varchar(255)," +
+						"lastMessage varchar(255))");
+						
+						// Create the message table
+						stmt.execute("CREATE TABLE message(id varchar(255),"
+								 + "previous varchar(255), creation BIGINT, author varchar(255), content varchar(255), nextMessage varchar(255))");
+						
+						// Give confirmation of execution.
+						System.out.println("Tables do not exist. Table creation executed.");
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
 			ex.printStackTrace();
 		}
 	}
 	
-	public boolean checkUsernameExists(String name) throws SQLException {
-		PreparedStatement checkUserTest = conn.prepareStatement(checkUsername);
-		checkUserTest.setString(1, name);
-		ResultSet user = checkUserTest.executeQuery();
-		
-		return user == null;
+	public boolean checkForUsername(String username) throws SQLException {
+		return stmt.execute("SELECT * FROM chatuser WHERE password = '" + username + "'" );
 	}
 	
-	public String userLoginCheck(String name, String password) throws SQLException {
-		PreparedStatement checkValidUserTest = conn.prepareStatement(checkValidUser);
-		checkValidUserTest.setString(1, name);
-		checkValidUserTest.setString(2, password);
-		ResultSet user = checkValidUserTest.executeQuery();
-		
-		
-		return (user != null) ? user.getString(1) : null;
+	public boolean checkForPassword(String password) throws SQLException {
+		return stmt.execute("SELECT * FROM chatuser WHERE password = '" + password + "'" );
 	}
 	
 	public void addUser(User u) throws SQLException {
-		PreparedStatement addUser = conn.prepareStatement(addUserInfo);
-		addUser.setString(1, removeCharsInUuid(u.id.toString()));
-		addUser.setString(2, u.name);
-		addUser.setString(3, "test");
-		addUser.setLong(4, u.creation.inMs());
-		
-		addUser.executeUpdate();
-		
-		/*stmt.execute("INSERT INTO " + userTableName + " VALUES(" + "\'" + removeCharsInUuid(u.id.toString()) + "\'" +
-				"," + "\'" + u.name + "\'" + ", 'test', " + u.creation.inMs() + ")");*/
+		stmt.execute("INSERT INTO chatuser VALUES(" + "\'" + removeCharsInUuid(u.id.toString()) + "\'" +
+				"," + "\'" + u.name + "\'" + ", 'test', " + u.creation.inMs() + ")");
 	}
 	
 	public void addMessage(Message m) throws SQLException {
-		PreparedStatement addMessage = conn.prepareStatement(addMessageInfo);
-		addMessage.setString(1, removeCharsInUuid(m.id.toString()));
-		addMessage.setString(2, removeCharsInUuid(m.previous.toString()));
-		addMessage.setLong(3, m.creation.inMs());
-		addMessage.setString(4, removeCharsInUuid(m.author.toString()));
-		addMessage.setString(5, m.content);
-		addMessage.setString(6, removeCharsInUuid(m.next.toString()));
-		
-		addMessage.executeUpdate();
-		
-		
-		/*stmt.execute("INSERT INTO " + messageTableName + " VALUES(" + "\'" +  removeCharsInUuid(m.id.toString()) +  "\'" +
+		stmt.execute("INSERT INTO message VALUES(" + "\'" +  removeCharsInUuid(m.id.toString()) +  "\'" +
 				","  +  "\'" + removeCharsInUuid(m.previous.toString()) +  "\'" + "," +  m.creation.inMs() + "," 
-				+  "\'" + removeCharsInUuid(m.author.toString()) +  "\'" + "," +  "\'" + m.content +  "\'" + "," +  "\'" + removeCharsInUuid(m.next.toString()) +  "\'" + ")");*/
+				+  "\'" + removeCharsInUuid(m.author.toString()) +  "\'" + "," +  "\'" + m.content +  "\'" + "," +  "\'" + removeCharsInUuid(m.next.toString()) +  "\'" + ")");
 	}
 	
 	
@@ -150,62 +94,30 @@ public class DerbyStore {
 		StringBuilder usersInvolved = new StringBuilder();
 		
 		for (Uuid s : c.users) {
-			// Adding chat participants for a specific conversation to a table specifically for it.
-			PreparedStatement addChatParticipants = conn.prepareStatement(addChatParticipantsInfo);
-			addChatParticipants.setString(1, removeCharsInUuid(c.id.toString()));
-			addChatParticipants.setString(2, removeCharsInUuid(s.toString()));
-			addChatParticipants.executeUpdate();
-			
-			//stmt.execute("INSERT INTO " + chatParticipantsTableName + " VALUES('" + removeCharsInUuid(c.id.toString()) + "','" + removeCharsInUuid(s.toString()) + "')");
-			System.out.println("INSERT INTO " + chatParticipantsTableName + " VALUES('" + removeCharsInUuid(c.id.toString()) + "','" + removeCharsInUuid(s.toString()) + "')");
+			usersInvolved.append(removeCharsInUuid(s.toString()));
+			usersInvolved.append(" ");
 		}
-
-		PreparedStatement addConversation = conn.prepareStatement(addConversationInfo);
-		addConversation.setString(1, removeCharsInUuid(c.id.toString()));
-		addConversation.setString(2, removeCharsInUuid(c.owner.toString()));
-		addConversation.setLong(3, c.creation.inMs());
-		addConversation.setString(4, c.title);
-		addConversation.setString(5, removeCharsInUuid(c.firstMessage.toString()));
-		addConversation.setString(6, removeCharsInUuid(c.lastMessage.toString()));
-		addConversation.executeUpdate();
 		
-		
-		/*stmt.execute("INSERT INTO " + conversationTableName + " VALUES(" +  "\'" + removeCharsInUuid(c.id.toString()) +  "\'" +
+		stmt.execute("INSERT INTO conversation VALUES(" +  "\'" + removeCharsInUuid(c.id.toString()) +  "\'" +
 				"," +  "\'" + removeCharsInUuid(c.owner.toString()) +  "\'" + "," + c.creation.inMs() + "," + "\'" + c.title + "\'" + "," +  "\'" + usersInvolved.toString() +  "\'" + 
-				"," +  "\'" + removeCharsInUuid(c.firstMessage.toString()) +  "\'" +  "," + "\'" + removeCharsInUuid(c.lastMessage.toString()) +  "\'" + ")");*/
+				"," +  "\'" + removeCharsInUuid(c.firstMessage.toString()) +  "\'" +  "," + "\'" + removeCharsInUuid(c.lastMessage.toString()) +  "\'" + ")");
 	}
 	
 	public void updateConversation(Conversation c) throws SQLException {
-		
-		PreparedStatement updateConversationStatement = conn.prepareStatement(updateConversationById);
-		
-		updateConversationStatement.setString(1, removeCharsInUuid(c.firstMessage.toString()));
-		updateConversationStatement.setString(2, removeCharsInUuid(c.lastMessage.toString()));
-		updateConversationStatement.setString(3, removeCharsInUuid(c.id.toString()));
-		
-		updateConversationStatement.executeUpdate();
-		
-		/*stmt.execute("UPDATE " + conversationTableName + " SET firstMessage = " + "\'" + removeCharsInUuid(c.firstMessage.toString()) 
-			+ "\'" + ", lastMessage = " + "\'" + removeCharsInUuid(c.lastMessage.toString()) + "\'" + " WHERE id = " + "\'" + removeCharsInUuid(c.id.toString()) + "\'");*/
+		stmt.execute("UPDATE conversation SET firstMessage = " + "\'" + removeCharsInUuid(c.firstMessage.toString()) 
+			+ "\'" + ", lastMessage = " + "\'" + removeCharsInUuid(c.lastMessage.toString()) + "\'" + " WHERE id = " + "\'" + removeCharsInUuid(c.id.toString()) + "\'");
 	}
 	
 	public void updateLastMessage(Message m) throws SQLException {
-		PreparedStatement updateMessageStatement = conn.prepareStatement(updateLastMessageById);
-		
-		updateMessageStatement.setString(1, removeCharsInUuid(m.next.toString()));
-		updateMessageStatement.setString(2, removeCharsInUuid(m.id.toString()));
-		
-		updateMessageStatement.executeUpdate();
-		
-		/*stmt.execute("UPDATE message SET nextMessage = " + "\'" + removeCharsInUuid(m.next.toString()) + "\'" + 
-				" WHERE id = " + "\'" + removeCharsInUuid(m.id.toString()) + "\'");*/
+		stmt.execute("UPDATE message SET nextMessage = " + "\'" + removeCharsInUuid(m.next.toString()) + "\'" + 
+				" WHERE id = " + "\'" + removeCharsInUuid(m.id.toString()) + "\'");
 	}
 	
 	public Store<Uuid, User> getAllUsers() {
 		Store<Uuid, User> allUsers = new Store<>(UUID_COMPARE);
 		try {
 			// Get all of the users in the database
-			ResultSet allUsersResponse = stmt.executeQuery("SELECT * FROM " + userTableName);
+			ResultSet allUsersResponse = stmt.executeQuery("SELECT * FROM chatuser");
 			
 			
 			while (allUsersResponse.next()) {
@@ -225,7 +137,7 @@ public class DerbyStore {
 				// Add each created user to the store object
 				allUsers.insert(userid, user);
 			}
-			LOG.info("Accessed users table.");
+			System.out.println("Successfully retrieved users");
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
@@ -238,24 +150,14 @@ public class DerbyStore {
 		Store<Uuid, Conversation> allConversations = new Store<>(UUID_COMPARE);
 		
 		try {
-			final Statement partipantsStatement = conn.createStatement();
+			ResultSet allConversationsResponse = stmt.executeQuery("SELECT * FROM conversation");
 			
-			ResultSet allConversationsResponse = stmt.executeQuery("SELECT * FROM " + conversationTableName);
 			
 			HashSet<Uuid> ownersUuid = new HashSet<>();
 			
 			while (allConversationsResponse.next()) {
 				
 				Uuid conversationid = Uuid.fromString(removeCharsInUuid(allConversationsResponse.getString(1)));
-				
-				// Retrieve the users that are a part of the conversation
-				ResultSet chatParticipants = partipantsStatement.executeQuery("SELECT userid FROM " + chatParticipantsTableName + " WHERE conversationid = '" + allConversationsResponse.getString(1) + "'");
-				
-				// Iterate over the participants and them to the hashset
-				while (chatParticipants.next()) {
-					ownersUuid.add(Uuid.fromString(chatParticipants.getString(1)));
-				}
-				
 				Uuid ownerUuid = Uuid.fromString(allConversationsResponse.getString(2));
 				Time creation = new Time(allConversationsResponse.getLong(3));
 				String title = allConversationsResponse.getString(4);
@@ -263,16 +165,16 @@ public class DerbyStore {
 				Uuid firstMessage = Uuid.fromString(allConversationsResponse.getString(6));
 				Uuid lastMessage = Uuid.fromString(allConversationsResponse.getString(7));
 				
-				/*for (String uuid : owners.split(" ")) {
+				for (String uuid : owners.split(" ")) {
 					if (!uuid.isEmpty()) ownersUuid.add(Uuid.fromString(uuid));
-				}*/
+				}
 				
 				
 				Conversation c = new Conversation(conversationid, ownerUuid, creation, title, ownersUuid, firstMessage, lastMessage);
 				allConversations.insert(conversationid, c);
 	
 			}
-			LOG.info("Accessed conversations table.");
+			System.out.println("Successfully retrieved conversations");
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
@@ -285,7 +187,7 @@ public class DerbyStore {
 		Store<Uuid, Message> allMessages = new Store<>(UUID_COMPARE);
 		
 		try {
-			ResultSet allMessagesResponse = stmt.executeQuery("SELECT * FROM " + messageTableName);
+			ResultSet allMessagesResponse = stmt.executeQuery("SELECT * FROM message");
 
 			while (allMessagesResponse.next()) {
 				
@@ -300,7 +202,7 @@ public class DerbyStore {
 				allMessages.insert(messageid, m);
 					
 			}
-			LOG.info("Accessed messages table.");
+			System.out.println("Successfully retrieved messages");
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
@@ -309,11 +211,6 @@ public class DerbyStore {
 		return allMessages;
 	}
 	
-	// When Uuid is converted to a string
-	// it adds characters such as opening
-	// and closing brackets. This function removes
-	// them, and decreases the different numbers in 
-	// the uuid since it could be larger than an int.
 	public String removeCharsInUuid(String uuid) {
 		// Remove the characters when uuid is transformed to a string
 		uuid = uuid.replace("[", "");
--- src/codeu/chat/server/Controller.java
+++ src/codeu/chat/server/Controller.java
@@ -78,12 +78,16 @@ public final class Controller implements RawController, BasicController {
       } else {
         final Message lastMessage = model.messageById().first(foundConversation.lastMessage);
         lastMessage.next = message.id;
-  
-        // EDIT - Malik Graham
-        // Calling a function to save the message
-        // links in the database.
-        model.updateMessageLink(lastMessage);
+       
         
+        // Updating the next message for the last message
+        try {
+        	model.ds.updateLastMessage(lastMessage);
+        }
+        catch (Exception ex) {
+        	ex.printStackTrace();
+        }
+       
       }
 
       // If the first message points to NULL it means that the conversation was empty and that
@@ -105,9 +109,13 @@ public final class Controller implements RawController, BasicController {
     }
     
     // EDIT - Malik Graham
-    // Calling a function to save the first and
-    // last message for a conversation in the database.
-    model.updateConversation(foundConversation);
+    // Update the conversations first and last message
+    try {
+    	model.ds.updateConversation(foundConversation);
+    }
+    catch (Exception ex) {
+    	ex.printStackTrace();
+    }
 
     return message;
   }
