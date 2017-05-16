a307b4e947a784ea0b36378ff8a071871feae518
Rubest <ruban_hussain@brown.edu>
2017-05-16 13:29:45 -0400

    restructure and cleaning up of Main.java for clarity and ease

--- src/main/java/com/codeu/Main.java
+++ src/main/java/com/codeu/Main.java
@@ -1,78 +1,5 @@
 package com.codeu;
 
-// import org.glassfish.grizzly.http.server.HttpServer;
-// import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-// import org.glassfish.jersey.server.ResourceConfig;
-
-// import java.io.IOException;
-// import java.net.URI;
-
-
-// import codeu.chat.server.Server;
-
-
-// /**
-//  * Main class.
-//  * Used to create and start the backend server
-//  * 
-//  * Based off of resources given at: 
-//  * https://jersey.java.net/documentation/latest/getting-started.html#new-from-archetype
-//  * 
-//  * 
-//  */
-// public class Main {
-//     // Base URI the Grizzly HTTP server will listen on
-//     public static final String BASE_URI = "http://localhost:8080/myapp/";
-
-//     /**
-//      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-//      * @return Grizzly HTTP server.
-//      */
-//     public static HttpServer startServer() {
-//         // create a resource config that scans for JAX-RS resources and providers
-//         // in this com.codu package
-//         final ResourceConfig rc = new ResourceConfig().packages("com.codeu");
-
-//         // this line allows programs running on other ports to read what is 
-//         // being sent from here (not good in the real world for cyber safety
-//         // reasons, but impt while developing)
-//         rc.register(new MyCORSFilter());
-
-//         // create and start a new instance of grizzly http server
-//         // exposing the Jersey application at BASE_URI
-//         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-//     }
-
-//     /**
-//      * Main method.
-//      * @param args
-//      * @throws IOException
-//      */
-//     public static void main(String[] args) throws IOException {
-//         final HttpServer server = startServer();
-//         System.out.println(String.format("Jersey app started with WADL available at "
-//                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-//         System.in.read();
-//         server.stop();
-//     }
-// }
-
-// Copyright 2017 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
-
 import java.io.IOException;
 
 import codeu.chat.common.Relay;
@@ -100,10 +27,45 @@ import java.net.URI;
 
 
 
-
+/**
+ * Main class.
+ * Used to create and start the backend server(s)
+ * Much of the code here is copied directly from what was once codeu.chat.ServerMain,
+ * with additions to set up connections with a web client.
+ * 
+ * Web client-related things based off of resources given at: 
+ * https://jersey.java.net/documentation/latest/getting-started.html#new-from-archetype
+ * 
+ * Other, copied code is Copyright 2017 Google Inc. under this license: http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ */
 final class Main {
 
   private static final Logger.Log LOG = Logger.newLog(Main.class);
+
+  // Base URI the Grizzly HTTP server will listen on (for the web client)
+  public static final String BASE_URI = "http://localhost:8080/myapp/";
+
+  /**
+   * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+   * This is for the web client to be able to access the java backend
+   * @return Grizzly HTTP server.
+   */
+  public static HttpServer startServer() {
+    // create a resource config that scans for JAX-RS resources and providers
+    // in this com.codu package
+    final ResourceConfig rc = new ResourceConfig().packages("com.codeu");
+
+    // this line allows programs running on other ports to read what is 
+    // being sent from here (not good in the real world for cyber safety
+    // reasons, but impt while developing)
+    rc.register(new MyCORSFilter());
+
+    // create and start a new instance of grizzly http server
+    // exposing the Jersey application at BASE_URI
+    return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+  }
+
 
   public static void main(String[] args) {
 
@@ -155,20 +117,16 @@ final class Main {
     LOG.info("Created server.");
 
 
-
-    String BASE_URI = "http://localhost:8080/myapp/";
-    final ResourceConfig rc = new ResourceConfig().packages("com.codeu");  
-    rc.register(new MyCORSFilter());
-    HttpServer webserver = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    // Starting server that can be accessed through get and post requests by the web client
+    final HttpServer webserver = startServer();
     System.out.println(String.format("Jersey app started with WADL available at "
-            + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+            + "%sapplication.wadl\n", BASE_URI));
+    // Server is not properly closed for now only
     // System.in.read();
     // webserver.stop();
 
 
-
     while (true) {
-
       try {
 
         LOG.info("Established connection...");
