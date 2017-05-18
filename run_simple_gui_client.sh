a11bddf02a5cecac806f5c1f8baa09486e740d1d
Sarah Depew <sdepew@brynmawr.edu>
2017-05-17 18:21:09 -0600

    Changed Server Code in run_simple_gui_client.sh to Reflect New Server

--- run_simple_gui_client.sh
+++ run_simple_gui_client.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#LOCAL_MACHINE="130.211.140.178@10006"
-
 LOCAL_MACHINE="130.211.140.178@10140"
 
 cd './bin'
