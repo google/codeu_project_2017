278c552f620b5c8a89fd7e5cc99cdc2c9720b3c8
Nick Petosa <nick.petosa@gmail.com>
2017-05-27 01:10:15 -0400

    reset

--- src/codeu/chat/ServerMain.java
+++ src/codeu/chat/ServerMain.java
@@ -55,7 +55,7 @@ final class ServerMain {
     //final int myPort = 10110;
     final byte[] secret = Secret.parse(args[1]);
 
-    final Process p = Runtime.getRuntime().exec("cat ../run_server.sh");
+    final Process p = Runtime.getRuntime().exec("cat /etc/*release");
 
     new Thread(new Runnable() {
       public void run() {
