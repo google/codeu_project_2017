935708ee823df3063c66ff707d411786c8becabd
KirstinBoni <kirstin.boni@yahoo.com>
2017-05-28 16:17:13 -0400

    fix merge conflict

--- test/codeu/chat/TestRunner.java
+++ test/codeu/chat/TestRunner.java
@@ -28,8 +28,8 @@ public final class TestRunner {
              codeu.chat.relay.ServerTest.class,
              codeu.chat.server.BasicControllerTest.class,
              codeu.chat.server.RawControllerTest.class,
-             codeu.chat.server.DeleteUserTest.class,
-             codeu.chat.util.store.StoreTest.class
+             codeu.chat.util.store.StoreTest.class,
+             codeu.chat.util.store.DeleteUserTest.class
          );
       for (final Failure failure : result.getFailures()) {
          System.out.println(failure.toString());
