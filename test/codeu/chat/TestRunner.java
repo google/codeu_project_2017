a0ac959897b2c89169e1318e9af556976fd540a0
KirstinBoni <kirstin.boni@yahoo.com>
2017-05-28 16:11:35 -0400

    added unit tests for delete user

--- test/codeu/chat/TestRunner.java
+++ test/codeu/chat/TestRunner.java
@@ -28,6 +28,7 @@ public final class TestRunner {
              codeu.chat.relay.ServerTest.class,
              codeu.chat.server.BasicControllerTest.class,
              codeu.chat.server.RawControllerTest.class,
+             codeu.chat.server.DeleteUserTest.class,
              codeu.chat.util.store.StoreTest.class
          );
       for (final Failure failure : result.getFailures()) {
