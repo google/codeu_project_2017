52dbf233e0463b42baafa37ccd0bba0aa87b61ac
Sarah Depew <sdepew@brynmawr.edu>
2017-05-29 19:09:02 -0600

    Team Meeting Changes

--- src/codeu/chat/common/User.java
+++ src/codeu/chat/common/User.java
@@ -41,13 +41,13 @@ public final class User {
     public User read(InputStream in) throws IOException {
 
       return new User(
-              Uuid.SERIALIZER.read(in),
-              Serializers.STRING.read(in),
-              Time.SERIALIZER.read(in),
-              Serializers.STRING.read(in)
-              );
+        Uuid.SERIALIZER.read(in),
+        Serializers.STRING.read(in),
+        Time.SERIALIZER.read(in),
+        Serializers.STRING.read(in)
+      );
     }
-
+  };
 
   public final Uuid id;
   public final String name;
@@ -55,32 +55,30 @@ public final class User {
   public final Time creation;
 
   public User(Uuid id, String name, Time creation) {
-
+  
     this.id = id;
     this.name = name;
     this.creation = creation;
-
+    this.password = null; 
   }
 
   //create new constructors
   public User(Uuid id, String name, Time creation, String password){
-
+  
     this.id = id;
     this.name = name;
     this.creation = creation;
     this.password = password;
-
   }
 
   //Potentially get rid of setter
   public void setPassword(String password){
-
+  
     this.password = password;
   }
 
   public String getPassword(){
-
+  
     return this.password;
   }
-
 }
