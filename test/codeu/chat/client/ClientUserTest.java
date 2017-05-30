89cd31c951156b26eca40851da110c38af4dc0da
mganesh6 <mganesh@wellesley.edu>
2017-05-29 22:23:09 -0400

    Tried to use powermock

--- test/codeu/chat/client/ClientUserTest.java
+++ test/codeu/chat/client/ClientUserTest.java
@@ -13,13 +13,16 @@ import org.mockito.plugins.MockMaker;
 import org.mockito.InjectMocks;
 import codeu.chat.util.Time;
 
+import org.powermock.api.mockito.PowerMockito;
+
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.runners.Suite.SuiteClasses;
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
-
 
 import codeu.chat.common.BasicController;
 import codeu.chat.common.Conversation;
@@ -34,23 +37,20 @@ import codeu.chat.client.Controller;
 import codeu.chat.client.ClientUser;
 import codeu.chat.client.View;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(View.class)
 public final class ClientUserTest {
 
 	private ClientUser user;
-	private static final Collection<Uuid> EMPTY = Arrays.asList(new Uuid[0]);
+	private static Collection<Uuid> EMPTY = Arrays.asList(new Uuid[0]);
 
-	@Mock
-	private Controller controller;
+	Controller controller = PowerMockito.mock(Controller.class);
 
-	@Mock
-	private View view;
+	View view = PowerMockito.mock(View.class);
 
 	@Before
 	public void doBefore() {
 		user = new ClientUser(controller, view);
-
-
 	}
 
 	@Test
@@ -63,7 +63,7 @@ public final class ClientUserTest {
 		userList.add(new User(new Uuid(4), "SaraH", new Time(800)));
 		userList.add(new User(new Uuid(5), "SARAH", new Time(900)));
 
-		Mockito.when(view.getUsersExcluding(EMPTY)).thenReturn(userList);
+		PowerMockito.when(view.getUsersExcluding(EMPTY)).thenReturn(userList);
 
 		user.updateUsers();
 
