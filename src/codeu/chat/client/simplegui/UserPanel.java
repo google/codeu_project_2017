b5db00105df317103f3361cf6c70d89880f06c0a
Sarah Depew <sdepew@brynmawr.edu>
2017-05-30 20:28:33 -0600

    formatting for simplegui

--- src/codeu/chat/client/simplegui/UserPanel.java
+++ src/codeu/chat/client/simplegui/UserPanel.java
@@ -39,7 +39,6 @@ public final class UserPanel extends JPanel {
     initialize();
   }
 
-
   private void initialize() {
 
     // This panel contains from top to bottom; a title bar, a list of users,
@@ -65,7 +64,6 @@ public final class UserPanel extends JPanel {
     final GridBagConstraints titleUserC = new GridBagConstraints();
     titleUserC.gridx = 2;
     titleUserC.gridy = 0;
-    //titleUserC.anchor = GridBagConstraints.LINE_END;
 
     titlePanel.add(titleLabel, titleLabelC);
     titlePanel.add(Box.createHorizontalGlue(), titleGapC);
@@ -260,8 +258,4 @@ public final class UserPanel extends JPanel {
       usersList.addElement(u.name);
     }
   }
-
-
-
-
 }
